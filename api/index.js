4983459f3b7ea50ed71d992ea46472313bf87645
shawnbirring <shawnbirring21@gmail.com>
2024-01-31 11:52:35 -0800

    back

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
-// const http = require("http");
+const http = require("http");
 
-// const server = http.createServer((req, res) => {
-//   console.log(`Request made ${req}`);
-//   res.writeHead(200, { "Content-Type": "text/html" });
-//   res.write("<h1>Hello World</h1>");
-//   res.end();
-// });
+const server = http.createServer((req, res) => {
+  console.log(`Request made ${req}`);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello World</h1>");
+  res.end();
+});
 
-// const port = 3000;
+const port = 3000;
 
-// server.listen(port, () => {
-//   console.log("Server is running on port 3000");
-// });
+server.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
 
-// module.exports = server;
-module.exports = (req, res) => {
-  res.status(200).send("<h1>Hello World</h1>");
-};
+module.exports = server;
+// module.exports = (req, res) => {
+//   res.status(200).send("<h1>Hello World</h1>");
+// };
