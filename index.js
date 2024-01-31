9434d9bec9507adcabda8b0a758776d66a069e42
shawnbirring <shawnbirring21@gmail.com>
2024-01-31 12:57:05 -0800

    fixed format

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const server = http.createServer((req, res) => {
   const date = new Date();
   const urlObj = url.parse(req.url);
   const queryObj = querystring.parse(urlObj.query);
-
-  const name = queryObj.name || null;
+  const name = queryObj.name;
 
   if (name) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(
-      `<h1 style="color: blue;">Hello, ${name}! Date: ${date.toLocaleDateString()}</h1>`
+      `<p style="color: blue;">Hello ${name}, What a beautiful day. Server current date and time is * ${date.toString()}</p>`
     );
     res.end();
   } else {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`<h1>Hello, World!</h1>`);
+    res.write(`<h1>Hello, there, provide a name!</h1>`);
     res.end();
   }
 });
