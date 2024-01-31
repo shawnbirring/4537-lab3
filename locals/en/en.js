245933feaf67e2ae34f8c88b9f8bd53d723992f4
shawnbirring <shawnbirring21@gmail.com>
2024-01-31 13:10:17 -0800

    moved message to another folder

---
+++ locals/en/en.js
@@ -0,0 +1,4 @@
+// message.js
+const str = `Hello {name}, What a beautiful day. Server current date and time is {date}`;
+
+module.exports = str;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const url = require("url");
 const querystring = require("querystring");
 const dm = require("./modules/utils");
+const msg = require("./locals/en/en");
 
 const server = http.createServer((req, res) => {
   const urlObj = url.parse(req.url);
@@ -9,10 +10,12 @@ const server = http.createServer((req, res) => {
   const name = queryObj.name;
 
   if (name) {
+    const date = dm.getDate();
+    const message = messageTemplate
+      .replace("{name}", name)
+      .replace("{date}", date);
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(
-      `<p style="color: blue;">Hello ${name}, What a beautiful day. Server current date and time is * ${dm.getDate()}</p>`
-    );
+    res.write(`<p style="color: blue;">${message}</p>`);
     res.end();
   } else {
     res.writeHead(200, { "Content-Type": "text/html" });
