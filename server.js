1c6e4b02aa332db9da15e2f77ee272a4b9899467
shawnbirring <shawnbirring21@gmail.com>
2024-01-31 11:34:45 -0800

    added console.log

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Request made");
+  console.log(`Request made ${req}`);
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<h1>Hello World</h1>");
   res.end();
 });
 
-server.listen(3000, () => {
+const port = 3000;
+
+server.listen(port, () => {
   console.log("Server is running on port 3000");
 });
