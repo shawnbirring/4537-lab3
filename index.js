594df49fa62fe7cef5fc610b53fc737a09538c8b
shawnbirring <shawnbirring21@gmail.com>
2024-01-31 12:32:38 -0800

    switched index to root

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello World</h1>");
+  res.end();
+});
+
+// server.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
+
+module.exports = server;
