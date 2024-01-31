6b3e71e9f988aa308236b5661eb03229d95c558f
shawnbirring <shawnbirring21@gmail.com>
2024-01-31 12:53:03 -0800

    added date and name

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const http = require("http");
+const url = require("url");
+const querystring = require("querystring");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello World</h1>");
-  res.end();
+  const date = new Date();
+  const urlObj = url.parse(req.url);
+  const queryObj = querystring.parse(urlObj.query);
+
+  const name = queryObj.name || null;
+
+  if (name) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+      `<h1 style="color: blue;">Hello, ${name}! Date: ${date.toLocaleDateString()}</h1>`
+    );
+    res.end();
+  } else {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`<h1>Hello, World!</h1>`);
+    res.end();
+  }
 });
 
 // server.listen(3000, () => {
